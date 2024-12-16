ee200a212edb522730a34d8b67b60d1690d7c9c1
Sktaabish <112170258+Sktaabish@users.noreply.github.com>
2024-12-16 21:50:37 +0530

    Add files via upload

---
+++ include/footer2.php
@@ -0,0 +1,48 @@
+<!-- FOOTER_SECTION -->
+<div class="footer_section">
+  <!-- FOOTER -->
+  <div class="footer">
+    <!-- FCONTAINER1 -->
+    <div class="fcontainer1">
+      <img src="images/logo.png" alt="LOGO">
+      <h2>You Heal.</h2>
+      <p>"At Our Hospital, we are committed to providing our patients with the highest quality of healthcare. Our
+        dedicated team of medical professionals is comprised of experienced physicians, nurses, and support staff who
+        are dedicated to ensuring that each and every patient receives the best possible care.
+      </p>
+    </div>
+
+    <!-- FCONTAINER2 -->
+    <div class="fcontainer2">
+      <h3>GENERAL</h3>
+      <!-- LIST -->
+      <ul class="list">
+        <li><a href="home.php">Home</a></li>
+        <li><a href="about2.php">About us</a></li>
+        <li><a href="service.php">Service</a></li>
+        <li><a href="contact2.php">Contact us</a></li>
+      </ul>
+    </div>
+    <!-- FCONTAINER3 -->
+    <div class="fcontainer3">
+      <h3>SOCIAL</h3>
+      <!-- SOCIALLIST -->
+      <ul class="Sociallist">
+        <li><a href="https://www.instagram.com/" target="blank"> Instagram</a></li>
+        <li><a href="https://www.facebook.com/" target="blank">Facebook</a></li>
+        <li><a href="https://www.twitter.com/" target="blank">Twitter</a></li>
+        <!-- SOCIALS -->
+        <div class="Socials">
+          <li><img src="images/ins.png" class="logo1" alt="Instagram Logo"></li>
+          <li><img src="images/Facebook.png" alt="Facebook Logo"></li>
+          <li><img src="images/twitter.png" alt="Twitter Logo"></li>
+        </div>
+      </ul>
+    </div>
+  </div>
+</div>
+<script src="script.js"></script>
+<script src="app.js"></script>
+</body>
+
+</html>
